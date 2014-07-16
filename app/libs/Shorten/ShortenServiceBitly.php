e14218003857722bf821c46905a8fea0f1a3e764
W01fdale <vershinin.a@donnu.edu.ua>
2014-07-16 10:39:56 +0000

    modified:   app/libs/Shorten/ShortenServiceBitly.php 	modified:   composer.json

--- app/libs/Shorten/ShortenServiceBitly.php
+++ app/libs/Shorten/ShortenServiceBitly.php
@@ -4,16 +4,20 @@ namespace Shorten;
 
 use Contrib\Component\Service\Bitly\V3\BitlyApiFactory;
 use Contrib\Component\Service\Bitly\V3\Api\Links;
+use Contrib\Component\Service\Bitly\V3\Request\RestClient;
+use Contrib\Component\Service\Bitly\V3\Response\BitlyResponse;
+use Contrib\Component\Service\Bitly\V3\Response\Bitly;
 
 class ShortenServiceBitly implements ShortenInterface {
     protected $bitly;    
 
     public function __construct() {
-        $this->bitly = new BitlyApiFactory("85bd9ebc7e0229c8067cb980291a98d565cd9757")->getLinks('array');
+        $this->bitly = new BitlyApiFactory("85bd9ebc7e0229c8067cb980291a98d565cd9757");
+        $this->bitly = $this->bitly->getLinks();
     }
     
     public function shorten($url) {
-    	return 'shorten';//$this->bitly->(array("longUrl" => $url))["data"]["url"];
+    	return $this->bitly->shorten(array("longUrl" => $url))["url"];
     }
 }
 ?>
